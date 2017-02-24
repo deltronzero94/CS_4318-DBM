1e4673aac2b4b0d65b69ce1501a36158a99d52e5
Carlos Rios <kmzero94@gmail.com>
2017-02-24 09:36:24 -0600

    Added UML Diagrams (WIP) and main.java
    main.java is a simple test. Work still needed for the class UML

---
+++ src/main.java
@@ -0,0 +1,22 @@
+// Group Member:
+//     1) Carlos A. Rios
+//     2) Derek Jones
+// Date: 
+//   February 23, 2017
+//
+// Purpose: 
+//  Implementation of a prototype application designed to 
+//  search a database for the card game "Magic the Gathering"
+//  and create decks from those cards via clients interface/login,
+//  and allow administrators to add, remove, or modify current database.
+// 
+// 
+
+public class main
+{
+    public static void main (String args[])
+    {
+        System.out.println("Hello World\n");
+    }
+
+}
