2f9aea82b5a64491988c0dd2a2520581b5b93122
Carlos Rios <kmzero94@gmail.com>
2017-03-07 00:47:34 -0600

    Testing SQL

---
+++ Testing/main.java
@@ -0,0 +1,16 @@
+
+package testing.src;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class main
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+        MTGCard card = new MTGCard();
+
+        Gson gson = new Gson();
+    }
+}
